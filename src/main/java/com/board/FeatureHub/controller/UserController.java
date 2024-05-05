a21f7feca0f3a8e2bf0b87f8bb9cac22bd44c3ff
JuYeon-Jin <juuju.00b@gmail.com>
2024-05-05 17:39:44 +0900

    create JWT ver2

--- src/main/java/com/board/FeatureHub/controller/UserController.java
+++ src/main/java/com/board/FeatureHub/controller/UserController.java
@@ -1,17 +1,23 @@
 package com.board.FeatureHub.controller;
 
-import com.board.FeatureHub.service.UserService;
+import com.board.FeatureHub.service.TokenService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/test")
-    public void Test() {
+    @GetMapping("/getToken")
+    public void joinFromGoogle() {
         System.out.println("컨트롤러");
-        UserService userService = new UserService();
-        userService.Test();
+        TokenService tokenService = new TokenService();
+        System.out.println("createJWT = " + tokenService.createJWT("i00d"));
     }
+
+    @GetMapping("/getUserInfo")
+    public void getUserInfo() {
+
+    }
+
 
 }
--- src/main/java/com/board/FeatureHub/dto/OAuthUserDto.java
+++ src/main/java/com/board/FeatureHub/dto/OAuthUserDto.java
@@ -16,5 +16,6 @@ public class OAuthUserDto {
     private String given_name;
     private String picture;
     private String locale;
+    private String provider;
 
 }
